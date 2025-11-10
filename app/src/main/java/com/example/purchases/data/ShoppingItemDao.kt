f69810cb68b150823ede45a56ed898483bebe187
vkozhanova <kozhanova_vera@mail.ru>
2025-11-10 18:22:16 +0500

    P-repository

--- app/src/main/java/com/example/purchases/data/ShoppingItemDao.kt
+++ app/src/main/java/com/example/purchases/data/ShoppingItemDao.kt
@@ -1,9 +1,23 @@
 package com.example.purchases.data
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ShoppingItemDao {
+
+    @Query("SELECT * FROM items")
+    fun getAllItems(): Flow<List<ShoppingItem>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem(item: ShoppingItem)
+
+    @Delete
+    suspend fun deleteItem(item: ShoppingItem)
 
 
 }
--- app/src/main/java/com/example/purchases/data/ShoppingListDao.kt
+++ app/src/main/java/com/example/purchases/data/ShoppingListDao.kt
@@ -1,9 +1,21 @@
 package com.example.purchases.data
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ShoppingListDao{
 
+    @Query("SELECT * FROM lists")
+    fun getAllLists(): Flow<List<ShoppingList>>
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertList(list: ShoppingList)
+
+    @Delete
+    suspend fun deleteList(list: ShoppingList)
 }
